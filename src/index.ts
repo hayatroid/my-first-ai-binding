d28440b0f531c40ff35fd9e96780ea57449cbc93
hayatroid <hayatroid@gmail.com>
2025-12-05 15:46:36 +0000

    🧹

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,12 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export interface Env {
-	AI: Ai;
+  AI: Ai;
 }
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		const response = await env.AI.run('@cf/meta/llama-3.2-3b-instruct', {
-			prompt: 'What is the origin of the phrase Hello, World',
-		});
-		return new Response(JSON.stringify(response));
-	},
+  async fetch(request, env, ctx): Promise<Response> {
+    const response = await env.AI.run("@cf/meta/llama-3.2-3b-instruct", {
+      prompt: "What is the origin of the phrase Hello, World",
+    });
+    return new Response(JSON.stringify(response));
+  },
 } satisfies ExportedHandler<Env>;
